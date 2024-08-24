3d1bd58fab9d09797185d58f42b6a0354f8fbfa1
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-08-24 17:54:04 +0200

    Update BaseMessenger.php

--- src/BaseMessenger.php
+++ src/BaseMessenger.php
@@ -3,11 +3,15 @@ namespace TurboLabIt\Messengers;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 
 abstract class BaseMessenger
 {
     protected ?ResponseInterface $lastResponse = null;
 
-    public function __construct(protected array $arrConfig, protected HttpClientInterface $httpClient) {}
+    public function __construct(
+        protected array $arrConfig, protected HttpClientInterface $httpClient,
+        protected ParameterBagInterface $parameters 
+    ) {}
 }
